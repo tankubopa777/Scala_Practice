9888f7d6ca9c479be0f85c234d883adf20189cc7
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-01-18 21:47:22 +0700

    update scala again

---
+++ src/main/scala/part2oop/AbstractDataTypes.scala
@@ -0,0 +1,32 @@
+package part2oop
+
+object AbstractDataTypes extends App{
+  //abstract
+  abstract class Animal{
+    val creatureType: String
+    def eat: Unit
+  }
+
+  class Dog extends Animal{
+    override val creatureType: String = "Canine"
+    override def eat: Unit = println("crunch crunch")
+  }
+  //traits
+  trait Carnivore{
+    def eat(animal: Animal): Unit
+    val preferredMeal: String = "fresh meat"
+  }
+
+  trait ColdBlooded
+  class Crocodile extends Animal with Carnivore{
+    override val creatureType: String = "croc"
+    override def eat: Unit = println("nomnomnom")
+    override def eat(animal: Animal): Unit = println(s"I'm a croc and I'm eating ${animal.creatureType}")
+  }
+
+  //traits vs abstract classes
+  //1 - traits do not have constructor parameters
+  //2 - multiple traits may be inherited by the same class
+  //3 - traits = behavior, abstract class = "thing"
+  
+}
---
+++ src/main/scala/part2oop/Generics.scala
@@ -0,0 +1,54 @@
+package part2oop
+
+object Generics {
+
+    class MyList[+A] {
+      // use the type A
+      def add[B >: A](element: B): MyList[B] = ???
+      /*
+        A = Cat
+        B = Dog = Animal
+      */
+    }
+
+    class MyMap[Key, Value]
+
+    val listOfIntegers = new MyList[Int]
+    val listOfStrings = new MyList[String]
+
+    // generic methods
+    object MyList {
+      def empty[A]: MyList[A] = ???
+    }
+
+    val emptyListOfIntegers = MyList.empty[Int]
+
+    // variance problem
+    class Animal
+    class Cat extends Animal
+    class Dog extends Animal
+
+    // 1. yes, List[Cat] extends List[Animal] = COVARIANCE
+    class CovariantList[+A]
+    val animal: Animal = new Cat
+    val animalList: CovariantList[Animal] = new CovariantList[Cat]
+    // animalList.add(new Dog) ??? HARD QUESTION => we return a list of Animals
+
+    // 2. NO = INVARIANCE
+    class InvariantList[A]
+    val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+    // 3. Hell, no! CONTRAVARIANCE
+    class Trainer[-A]
+    val trainer: Trainer[Cat] = new Trainer[Animal]
+
+    // bounded types
+    class Cage[A <: Animal](animal: A) // class Cage only accepts type parameters that are subtypes of Animal
+    val cage = new Cage(new Dog)
+
+    class Car
+    // val newCage = new Cage(new Car) // this will not compile
+
+    // expand MyList to be generic
+
+}
